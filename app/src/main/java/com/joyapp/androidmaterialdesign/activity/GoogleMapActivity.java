51bd58ac0d53285049626a21c2ae9730e27aa990
nayeemjoy <porajitojoy@gmail.com>
2016-06-10 12:39:43 +0600

    Google Map Added Successfully

--- app/src/main/java/com/joyapp/androidmaterialdesign/activity/GoogleMapActivity.java
+++ app/src/main/java/com/joyapp/androidmaterialdesign/activity/GoogleMapActivity.java
@@ -10,11 +10,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.joyapp.androidmaterialdesign.R;
 public class GoogleMapActivity extends AppCompatActivity {
     private Toolbar mToolbar;
+    private GoogleMap googleMap;
+    private MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -33,7 +45,10 @@ public class GoogleMapActivity extends AppCompatActivity {
             }
         });
 
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
